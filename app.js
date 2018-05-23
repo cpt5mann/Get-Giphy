75e6157e863174903e5359dd1937ddc2e5cc2dd7
Carter <chalfmann9547@myhta.org>
2018-05-22 15:01:31 -1000

    working on it. Still need to figure out how to get the right urls.

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
-console.log("hello");
-
-var useinp=document.querySelector("#userinput");
+var useinp=document.querySelector("#userinput"); 
 var subbtn=document.querySelector("#submit");
 var formm=document.querySelector("#formm");
 
 formm.addEventListener("submit", function(event){
-
 event.preventDefault();
-console.log(useinp.value);
 });
+
 function reqListener(){
-    var data =JSON.parse(this.responseText);
+    var data = JSON.parse(this.responseText);
     console.log(data);
+   };
 
-};
+   var oReq= new XMLHttpRequest();
+   oReq.addEventListener("load", reqListener)
+   var half="http://api.giphy.com/v1/gifs/search?q=&api_key=0p2Ql693PCpiDSqLcJHdZKlWMEdAJkla"
+   oReq.open("GET", half);
+   oReq.send();
 
-var oReq= new XMLHttpRequest();
-oReq.addEventListener("load", reqListener)
-oReq.open("GET", "http://api.giphy.com/v1/gifs/search?q=" + useinp + "&api_key=0p2Ql693PCpiDSqLcJHdZKlWMEdAJkla")
-oReq.send();
-
-
-
-
-
-
-
+// I cant figure out how to change the url with the user input.
 
 
 
