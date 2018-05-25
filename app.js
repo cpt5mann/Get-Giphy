c8cb7d22be76c53072871b0089ed75d21f43e4cf
Carter <chalfmann9547@myhta.org>
2018-05-24 15:02:01 -1000

    done

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
 var useinp=document.querySelector("#userinput"); 
 var subbtn=document.querySelector("#submit");
 var formm=document.querySelector("#formm");
+var result=document.querySelector("#result")
+
+function reqListener(){
+    var stuff = JSON.parse(this.responseText);
+
+
+
+for (var i=0; i<stuff.data.length; i++){
+
+console.log(stuff.data[i]);
+var img=document.createElement("img");   
+img.src=stuff.data[i].images.original.url;
+result.appendChild(img);
+
+}
+};
+
+
+
+
 
 formm.addEventListener("submit", function(event){
 event.preventDefault();
-});
+console.log("hesaidit= " + useinp.value)
+var hesaidit=useinp.value;
 
-function reqListener(){
-    var data = JSON.parse(this.responseText);
-    console.log(data);
-   };
 
-   var oReq= new XMLHttpRequest();
+var oReq= new XMLHttpRequest();
    oReq.addEventListener("load", reqListener)
-   var half="http://api.giphy.com/v1/gifs/search?q=&api_key=0p2Ql693PCpiDSqLcJHdZKlWMEdAJkla"
-   oReq.open("GET", half);
+   var url="http://api.giphy.com/v1/gifs/search?q=" + hesaidit  +"&api_key=0p2Ql693PCpiDSqLcJHdZKlWMEdAJkla&limit=5"
+   oReq.open("GET", url);
    oReq.send();
 
-// I cant figure out how to change the url with the user input.
+
+});
+
+
+
+
+
+  
+
+
 
 
 
